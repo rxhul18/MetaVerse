7e24bc8cad27d5a25e9b1ea4e3699ac0649f2aa4
Rahul Shah <rahulshah0728@gmail.com>
2024-11-07 23:16:13 +0530

    Added all the tests & stuck between integrating the prisma orm with postgres db, created project with turboRepo

--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -1,4 +1,31 @@
 import { Router } from "express";
+import { userRouter } from "./user";
+import { spaceRouter } from "./space";
+import { adminRouter } from "./admin";
 
 export const router = Router();
 
+router.get('/signup',(req,res)=>{
+    res.json({
+        message:"SignUp"
+    })
+})
+
+router.get('/signin',(req,res)=>{
+    res.json({
+        message:"SignIn"
+    })
+})
+
+router.get('/elements',(req,res)=>{
+
+})
+
+
+router.get('/avatars',(req,res)=>{
+
+})
+
+router.use("/user",userRouter)
+router.use("/space",spaceRouter)
+router.use("/admin",adminRouter)
